2853001fa1812eb118491553814193604f6354bb
Sara Nuredinovski <saranur.cs@gmail.com>
2024-12-22 22:20:04 +0100

    Fix autoimport angular material components & Add Company Select Dropdown

--- Querynaut-Frontend/src/app/app.component.ts
+++ Querynaut-Frontend/src/app/app.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+
+import { _MatInternalFormField } from '@angular/material/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    FormsModule,
+    ReactiveFormsModule,
+    MatCheckboxModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'Querynaut-Frontend';
