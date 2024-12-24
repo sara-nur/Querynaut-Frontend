14c45292267f1c13128e04c6d8cbcab409300022
Sara Nuredinovski <saranur.cs@gmail.com>
2024-12-24 19:12:13 +0100

    Add basic Home Page setup

--- Querynaut-Frontend/src/app/app.component.ts
+++ Querynaut-Frontend/src/app/app.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatCheckboxModule } from '@angular/material/checkbox';
-
-import { _MatInternalFormField } from '@angular/material/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -22,5 +19,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'Querynaut-Frontend';
+  title = 'Querynaut';
 }
